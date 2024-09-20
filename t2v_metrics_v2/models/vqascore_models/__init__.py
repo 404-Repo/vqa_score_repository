37b9ac782b22e2672899c32af84c88fefadf4b2f
tesha <tereshin.tesha@gmail.com>
2024-09-20 13:30:32 +0100

    [UPDATED] removed all redundant code/data

---
+++ t2v_metrics_v2/models/vqascore_models/__init__.py
@@ -0,0 +1,19 @@
+from .clip_t5_model import CLIP_T5_MODELS, CLIPT5Model
+from ...constants import HF_CACHE_DIR
+
+
+ALL_VQA_MODELS = [
+    CLIP_T5_MODELS,
+]
+
+
+def list_all_vqascore_models():
+    return [model for models in ALL_VQA_MODELS for model in models]
+
+
+def get_vqascore_model(model_name, device='cuda', cache_dir=HF_CACHE_DIR, **kwargs):
+    assert model_name in list_all_vqascore_models()
+    if model_name in CLIP_T5_MODELS:
+        return CLIPT5Model(model_name, device=device, cache_dir=cache_dir, **kwargs)
+    else:
+        raise NotImplementedError()
