8c12544c28cd2a24db17c30cf8ceb3651f3de419
tesha <tereshin.tesha@gmail.com>
2024-09-25 15:47:17 +0100

    [UPDATED] refactored & optimized

--- t2v_metrics/clip_t5_model/multimodal_encoder/clip_encoder.py
+++ t2v_metrics/clip_t5_model/multimodal_encoder/clip_encoder.py
@@ -1,77 +1,78 @@
+import gc
+
 import torch
 import torch.nn as nn
-
-from transformers import CLIPVisionModel, CLIPImageProcessor, CLIPVisionConfig
+from transformers import CLIPVisionModel
 
 
 class CLIPVisionTower(nn.Module):
-    def __init__(self, vision_tower, args, delay_load=False):
+    def __init__(self, args, device: str = "cuda"):
         super().__init__()
 
-        self.is_loaded = False
+        self._select_layer = args.mm_vision_select_layer
+        self._select_feature = getattr(args, 'mm_vision_select_feature', 'patch')
+        self._device = device
 
-        self.vision_tower_name = vision_tower
-        self.select_layer = args.mm_vision_select_layer
-        self.select_feature = getattr(args, 'mm_vision_select_feature', 'patch')
+        self._model: CLIPVisionModel = None
 
-        if not delay_load:
-            self.load_model()
-        else:
-            self.cfg_only = CLIPVisionConfig.from_pretrained(self.vision_tower_name)
+    def load_model(self, model_name: str):
+        """"""
+        self._model = CLIPVisionModel.from_pretrained(model_name)
+        self._model.requires_grad_(False)
+        self._model.eval()
 
-    def load_model(self):
-        self.image_processor = CLIPImageProcessor.from_pretrained(self.vision_tower_name)
-        self.vision_tower = CLIPVisionModel.from_pretrained(self.vision_tower_name)
-        self.vision_tower.requires_grad_(False)
-        self.is_loaded = True
+    def unload_model(self):
+        """"""
+        del self._model
+        self._model = None
 
-    def feature_select(self, image_forward_outs):
-        image_features = image_forward_outs.hidden_states[self.select_layer]
-        if self.select_feature == 'patch':
+        gc.collect()
+        torch.cuda.empty_cache()
+
+    def feature_select(self, image_forward_outs: torch.Tensor) -> torch.Tensor:
+        """
+
+        Parameters
+        ----------
+        image_forward_outs
+
+        Returns
+        -------
+
+        """
+        image_features = image_forward_outs.hidden_states[self._select_layer]
+
+        if self._select_feature == 'patch':
             image_features = image_features[:, 1:]
-        elif self.select_feature == 'cls_patch':
+        elif self._select_feature == 'cls_patch':
             image_features = image_features
         else:
-            raise ValueError(f'Unexpected select feature: {self.select_feature}')
+            raise ValueError(f'Unexpected select feature: {self._select_feature}')
         return image_features
 
     @torch.no_grad()
-    def forward(self, images):
-        if type(images) is list:
-            image_features = []
-            for image in images:
-                image_forward_out = self.vision_tower(image.to(device=self.device, dtype=self.dtype).unsqueeze(0), output_hidden_states=True)
-                image_feature = self.feature_select(image_forward_out).to(image.dtype)
-                image_features.append(image_feature)
-        else:
-            image_forward_outs = self.vision_tower(images.to(device=self.device, dtype=self.dtype), output_hidden_states=True)
-            image_features = self.feature_select(image_forward_outs).to(images.dtype)
+    def forward(self, images: torch.tensor):
+        """
+
+        Parameters
+        ----------
+        images
+
+        Returns
+        -------
+
+        """
+        image_forward_out = self._model(images.to(device=self._device), output_hidden_states=True)
+        image_features = self.feature_select(image_forward_out).to(images.dtype)
 
         return image_features
 
     @property
-    def dummy_feature(self):
-        return torch.zeros(1, self.hidden_size, device=self.device, dtype=self.dtype)
-
-    @property
-    def dtype(self):
-        return self.vision_tower.dtype
-
-    @property
-    def device(self):
-        return self.vision_tower.device
-
-    @property
-    def config(self):
-        if self.is_loaded:
-            return self.vision_tower.config
-        else:
-            return self.cfg_only
-
-    @property
     def hidden_size(self):
-        return self.config.hidden_size
+        """"""
+        return self._model.config.hidden_size
 
     @property
     def num_patches(self):
-        return (self.config.image_size // self.config.patch_size) ** 2
+        """"""
+        return (self._model.config.image_size // self._model.config.patch_size) ** 2
