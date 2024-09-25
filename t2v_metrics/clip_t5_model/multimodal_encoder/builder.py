bc137e8b618892d420f736747a3db2b3e3d7e672
tesha <tereshin.tesha@gmail.com>
2024-09-25 15:46:39 +0100

    [UPDATED] refactored builder

--- t2v_metrics/clip_t5_model/multimodal_encoder/builder.py
+++ t2v_metrics/clip_t5_model/multimodal_encoder/builder.py
@@ -1,11 +1,26 @@
 import os
 from .clip_encoder import CLIPVisionTower
-
+# from .clip_encoder_v2 import CLIPVisionTower
 
 def build_vision_tower(vision_tower_cfg, **kwargs):
+    """
+
+    Parameters
+    ----------
+    vision_tower_cfg
+    kwargs
+
+    Returns
+    -------
+
+    """
+
     vision_tower = getattr(vision_tower_cfg, 'mm_vision_tower', getattr(vision_tower_cfg, 'vision_tower', None))
     is_absolute_path_exists = os.path.exists(vision_tower)
     if is_absolute_path_exists or vision_tower.startswith("openai") or vision_tower.startswith("laion"):
-        return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
-
-    raise ValueError(f'Unknown vision tower: {vision_tower}')
+        vision_tower_model = CLIPVisionTower(args=vision_tower_cfg)
+        # vision_tower_model.load_model("ViT-L-14-336", "openai")
+        vision_tower_model.load_model(model_name=vision_tower)
+        return vision_tower_model
+    else:
+        raise ValueError(f'Unknown vision tower: {vision_tower}')
