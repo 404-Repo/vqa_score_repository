77688cfcaa9426a14609478f53aaaa232a2579ba
teshaTe <tereshin.tesha@gmail.com>
2024-12-11 08:40:10 +0000

    [UPDATED] project restructure & img proc. optim.

---
+++ t2v_metrics/clip_t5_model/multimodal_encoder/clip_encoder.py
@@ -0,0 +1,77 @@
+import gc
+
+import torch
+import torch.nn as nn
+from transformers import CLIPVisionModel
+
+
+class CLIPVisionTower(nn.Module):
+    def __init__(self, args, device: str = "cuda"):
+        super().__init__()
+
+        self._select_layer = args.mm_vision_select_layer
+        self._select_feature = getattr(args, 'mm_vision_select_feature', 'patch')
+        self._device = device
+
+        self.vision_tower: CLIPVisionModel = None
+
+    def load_model(self, model_name: str):
+        """"""
+        self.vision_tower = CLIPVisionModel.from_pretrained(model_name)
+        self.vision_tower.requires_grad_(False)
+        self.vision_tower.eval()
+
+    def unload_model(self):
+        """"""
+        del self.vision_tower
+        self.vision_tower = None
+
+        gc.collect()
+        torch.cuda.empty_cache()
+
+    def feature_select(self, image_forward_outs: torch.Tensor) -> torch.Tensor:
+        """
+
+        Parameters
+        ----------
+        image_forward_outs
+
+        Returns
+        -------
+
+        """
+        image_features = image_forward_outs.hidden_states[self._select_layer]
+
+        if self._select_feature == 'patch':
+            image_features = image_features[:, 1:]
+        elif self._select_feature == 'cls_patch':
+            image_features = image_features
+        else:
+            raise ValueError(f'Unexpected select feature: {self._select_feature}')
+        return image_features
+
+    @torch.no_grad()
+    def forward(self, images: torch.tensor):
+        """
+
+        Parameters
+        ----------
+        images
+
+        Returns
+        -------
+
+        """
+        image_forward_out = self.vision_tower(images.to(device=self._device), output_hidden_states=True)
+        image_features = self.feature_select(image_forward_out).to(images.dtype)
+        return image_features
+
+    @property
+    def hidden_size(self):
+        """"""
+        return self.vision_tower.config.hidden_size
+
+    @property
+    def num_patches(self):
+        """"""
+        return (self.vision_tower.config.image_size // self.vision_tower.config.patch_size) ** 2
---
+++ t2v_metrics/visual_model.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+import torch
+
+
+class BaseVisualModel(ABC):
+
+    @abstractmethod
+    @torch.no_grad()
+    @torch.autocast(device_type='cuda', dtype=torch.bfloat16)
+    def forward(self, images: list[torch.Tensor] | torch.Tensor,
+                texts: list[str] | str,
+                question_template: str,
+                answer_template: str) -> torch.Tensor:
+        """
+
+        Parameters
+        ----------
+        images
+        texts
+        question_template
+        answer_template
+
+        Returns
+        -------
+
+        """
+        pass
+
+    @abstractmethod
+    def preload_model(self, model_name: str):
+        """
+
+        Parameters
+        ----------
+        model_name
+
+        Returns
+        -------
+
+        """
+        pass
+
+    @abstractmethod
+    def unload_model(self):
+        """"""
+        pass
+
