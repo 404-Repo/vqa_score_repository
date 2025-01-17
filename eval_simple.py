e26d9fc475e507feb787c51b5c812474263c718a
tesha <tereshin.tesha@gmail.com>
2025-01-17 17:07:26 +0000

    updated for validating multiple preview images

--- eval_simple.py
+++ eval_simple.py
@@ -20,18 +20,42 @@ def get_all_image_files(folder_path: str) -> list[Path]:
     return sorted_files
 
 
+def load_prompts(folder_path: Path) -> list[str]:
+    """
+    Function for loading prompts from the specified txt file
+
+    Parameters
+    ----------
+    folder_path: path to the file with prompts
+
+    Returns
+    -------
+    prompts_in: a list with preloaded prompts
+    """
+    prompt_file = list(folder_path.rglob("*.txt"))
+    if len(prompt_file) == 0:
+        prompt_file = list(folder_path.parent.rglob("*.txt"))
+    else:
+        print(f"Cannot find txt file with prompts in folder: {folder_path} or {folder_path.parent}")
+
+    with prompt_file[0].open() as file:
+        prompts_in = [line.rstrip() for line in file]
+
+    return prompts_in
+
+
 def main():
-    img_folder = ""
-    prompt = ""
+    img_folder = "./test_data"
+
+    prompts = load_prompts(img_folder)
 
     cache_dir = t2v_metrics.constants.HF_CACHE_DIR
     score_func = t2v_metrics.get_score_model(model="llava-v1.6-13b", device="cuda", cache_dir=cache_dir)
 
     imgs_paths = get_all_image_files(img_folder)
-    scores = score_func.forward(imgs_paths, prompt)
-
-    print("Scores: ", scores)
-    print("Score: ", scores.mean())
+    for img_file, prompt in zip(imgs_paths, prompts):
+        score = score_func.forward([imgs_file], [prompt])
+        print("Scores: ", score)
 
 
 if __name__ == '__main__':
