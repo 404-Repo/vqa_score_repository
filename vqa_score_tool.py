794146f8e79b55bcc948caf32b225fc13836cc21
tesha <tereshin.tesha@gmail.com>
2025-01-28 11:26:19 +0000

    [UPDATED] basic usage, the rest commented out temp.

--- vqa_score_tool.py
+++ vqa_score_tool.py
@@ -9,13 +9,15 @@ from PIL import Image
 
 if __name__ == '__main__':
     image_path = "./images/image.png"
-    prompt1 = "two dogs running in the forest"
+    prompt1 = "two dogs chasing each other in the forest"
 
     image = np.array(Image.open(image_path))
     torch_image = torch.tensor(image)
 
     model = t2v_metrics.VQAScore()
-    model.preload_model("clip-flant5-xl")
+    # model.preload_model("smolvlm-1.7b-synth")
+    # model.preload_model("llava-v1.5-7b")
+    # model.preload_model("cogvlm-17b")
 
     t1 = time()
     score1 = model([torch_image], [prompt1])
@@ -25,23 +27,23 @@ if __name__ == '__main__':
     print(f" Input prompt: {prompt1}")
     print(f" VQAScore: {score1}")
 
-    # image_folder = Path("/home/tesha/Documents/Python/three-gen-subnet-private/validation/benchmark/benchmark_output/low_quality/images/happy_imp_")
-    image_folder = Path("/home/tesha/Documents/Python/three-gen-subnet-private/validation/benchmark/benchmark_output/medium_quality/images/happy_imp")
-    # prompt2 = "oak tree low poly"
-    prompt2 = "happy imp"
-
-    images_files = list(image_folder.rglob("*.png"))
-    images = []
-    for img_file in images_files:
-        image = torch.tensor(np.array(Image.open(img_file)))
-        images.append(image)
-
-    t1 = time()
-    score2 = model(images, [prompt2])
-    t2 = time()
-
-    score2 = np.exp(np.log(score2).mean())
-
-    print(f" It took: {t2 - t1} s")
-    print(f" Input prompt: {prompt2}")
-    print(f" VQAScore: {score2}")
+    # # image_folder = Path("/home/tesha/Documents/Python/three-gen-subnet-private/validation/benchmark/benchmark_output/low_quality/images/happy_imp_")
+    # image_folder = Path("/home/tesha/Documents/Python/three-gen-subnet-private/validation/benchmark/benchmark_output/medium_quality/images/happy_imp")
+    # # prompt2 = "oak tree low poly"
+    # prompt2 = "happy imp"
+    #
+    # images_files = list(image_folder.rglob("*.png"))
+    # images = []
+    # for img_file in images_files:
+    #     image = torch.tensor(np.array(Image.open(img_file)))
+    #     images.append(image)
+    #
+    # t1 = time()
+    # score2 = model(images, [prompt2])
+    # t2 = time()
+    #
+    # score2 = np.exp(np.log(score2).mean())
+    #
+    # print(f" It took: {t2 - t1} s")
+    # print(f" Input prompt: {prompt2}")
+    # print(f" VQAScore: {score2}")
