b136a4d036a9e9426cd84b8ab70067559c39039e
tesha <tereshin.tesha@gmail.com>
2025-01-30 15:38:53 +0000

    [UPDATED] cleaned

--- t2v_metrics/vqascore.py
+++ t2v_metrics/vqascore.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 
 from t2v_metrics.models.clip_t5_model.clip_t5_model import CLIPT5Model
-from t2v_metrics.models.smolvlm_model import SmolVLMModel
+from t2v_metrics.models.qwen2_vl_model import QwenVLModel
 from t2v_metrics.models.llava_model import LLaVAModel
 
 
@@ -53,16 +53,14 @@ class VQAScore(nn.Module):
         -------
 
         """
-        if model_name == "smolvlm-1.7b-base" or model_name == "smolvlm-1.7b-synth":
-            self._model = SmolVLMModel()
-            self._model.preload_model(model_name)
-        elif model_name == "clip-flant5-xxl" or model_name == "clip-flant5-xl":
+        if model_name == "clip-flant5-xxl" or model_name == "clip-flant5-xl":
             self._model = CLIPT5Model()
-            self._model.preload_model(model_name)
         elif model_name == "llava-v1.5-7b" or model_name == "llava-v1.5-13b":
             self._model = LLaVAModel()
-            self._model.preload_model(model_name)
+        elif model_name == "qwen2-vl-2b":
+            self._model = QwenVLModel()
 
+        self._model.preload_model(model_name)
 
     def unload_model(self):
         """"""
