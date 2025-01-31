5c30a38e747d79196f360f659f51c5ca33b6698a
tesha <tereshin.tesha@gmail.com>
2025-01-31 16:31:50 +0000

    [UPDATED] preload func. with new filed for quantization -> not in use

--- t2v_metrics/visual_model.py
+++ t2v_metrics/visual_model.py
@@ -32,7 +32,7 @@ class BaseVisualModel(ABC):
         pass
 
     @abstractmethod
-    def preload_model(self, model_name: str, torch_type: torch.dtype | None = None):
+    def preload_model(self, model_name: str, quant_type: dict = {}):
         """
 
         Parameters
